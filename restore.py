d1ca1f694ce7d5a4d548f96e8ef7822cfd713a22
XZS <d.f.fischer@web.de>
2014-11-24 03:10:30 +0100

    switch to class architecture
    Extending a class enables the addition of custom arguments.

--- restore.py
+++ restore.py
@@ -8,17 +8,21 @@ from csv import DictReader
 from progress import progress_file, BracketBar
 from progressbar import Percentage
 
-def exec(db, file):
-    pbar = progress_file(file, BracketBar(), Percentage())
-    for row in DictReader(file):
-        id = db.execute("SELECT id FROM Media WHERE key='{}' AND value='{}'"
-                .format(key, row[key])).fetchone()[0]
-        for field in values:
-            db.execute(
-                    "UPDATE Media SET value='{value}', intval={value} WHERE key='{field}' AND id={id}"
-                    .format(value=row[field], field=field, id=id))
-        pbar.update(file.buffer.tell())
-    pbar.finish()
+class Restore(MLibCSVAdapter):
+    def __init__(self):
+        super().__init__('r', "Read from a given file instead of the standard input.")
+
+    def exec(self, db, file):
+        pbar = progress_file(file, BracketBar(), Percentage())
+        for row in DictReader(file):
+            id = db.execute("SELECT id FROM Media WHERE key='{}' AND value='{}'"
+                    .format(key, row[key])).fetchone()[0]
+            for field in values:
+                db.execute(
+                        "UPDATE Media SET value='{value}', intval={value} WHERE key='{field}' AND id={id}"
+                        .format(value=row[field], field=field, id=id))
+            pbar.update(file.buffer.tell())
+        pbar.finish()
 
 if __name__ == '__main__':
-    MLibCSVAdapter('r', "Read from a given file instead of the standard input.").run(exec)
+    Restore().run()
